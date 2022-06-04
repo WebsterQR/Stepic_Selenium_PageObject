a5e355933bc54df88bb278a3fcac330f01126909
KoverevEvgeniy <koverev.evg@gmail.com>
2022-06-05 02:36:02 +0300

    tests for product page

---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+import math
+
+from selenium.common import NoAlertPresentException
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        link = self.browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button")
+        link.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
@@ -8,8 +9,11 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def test_guest_should_see_login_link(browser):
-    #link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
